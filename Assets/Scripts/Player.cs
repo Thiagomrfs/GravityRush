dd4a4f8ad33b97e07855f241bf90c21d35de5eac
Thiagomrfs <thiagomarbsn@gmail.com>
2022-09-23 23:43:52 -0300

    player death

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private float leftEdge;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
             rb.gravityScale *= -1;
+        }
+
+        if (transform.position.x < leftEdge) {
+            SceneManager.LoadScene("SampleScene");
         }
     }
 

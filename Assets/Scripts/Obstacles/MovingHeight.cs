ede2d9ba39b348fd411f0ad2924ebf7ecbae02ba
Thiagomrfs <thiagomarbsn@gmail.com>
2022-09-24 20:14:11 -0300

    moving height obstacle

---
+++ Assets/Scripts/Obstacles/MovingHeight.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingHeight : MonoBehaviour
+{
+    private float min = 6;
+    private float max = 16;
+    private float height;
+    void Start()
+    {
+        height = Random.Range(min, max);
+        transform.localScale = new Vector3(1, height, 1);
+        StartCoroutine(HeightChange());
+    }
+    
+    void Update()
+    {
+        transform.localScale = Vector3.Lerp(
+            transform.localScale, 
+            new Vector3(1, height, 1), 
+            6f * Time.deltaTime
+        );
+    }
+
+    private IEnumerator HeightChange()
+    {
+        yield return new WaitForSeconds(1);
+        height = Random.Range(min, max);
+        StartCoroutine(HeightChange());
+    }
+}
